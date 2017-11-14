f0be21c3a51b7f0d0c6a223ae62f00d3e2ba0485
Marcos Mohar <elcabeca@gmail.com>
2017-11-13 20:47:03 -0600

    added jwt authentication to settings

--- espotifail/settings.py
+++ espotifail/settings.py
@@ -130,6 +130,17 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
+
 COUNTRIES = (
     ("ALB", "Albania"),
     ("DZA", "Algeria"),
