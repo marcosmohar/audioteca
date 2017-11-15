c3c7cb5abb4eac110144c7eff5fef6253f10e125
Marcos Mohar <elcabeca@gmail.com>
2017-11-14 21:11:55 -0600

    attempted filtering queries without much success

--- modules/artists/views.py
+++ modules/artists/views.py
@@ -5,15 +5,31 @@ from rest_framework.response import Response
 from .models import Artist
 from .serializers import ArtistModelSerializer
 from django.http import Http404
+from django.db.models import Q
 # Create your views here.
 
 class ListArtist(APIView):
 
+    def _filtering(self, params):
+        genre = params.get('genre', "")
+        band = params.get('band', False)
+        name = params.get('name', "")
+
+        queryset = Artist.objects.filter(primary_genre__iexact=genre, is_band=band, name__icontains=name)
+        return queryset
+
     def get(self, request):
         """ Returns serialized artist data"""
-        artists = Artist.objects.all()
-        serializer = ArtistModelSerializer(artists, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        if request.query_params:
+            query = self._filtering(request.query_params)
+            serializer = ArtistModelSerializer(query, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+                
+            artists = Artist.objects.all()
+            serializer = ArtistModelSerializer(artists, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         """ Save data to DB or returns an error if data is not valid"""
